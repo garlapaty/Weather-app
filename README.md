fd085d66f75e6d3ce3ac8cb8b79fb0c14de0170f
garlapaty <suprajagarlapaty@gmail.com>
2019-04-26 23:21:33 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Weather-app
-This is a weather app which takes a weather API to show the weather info for a city.
---
+++ precache-manifest.e441467cb8fda8a2d91f5d3331edb805.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169fffd1b94cba589ef24a7f24ef089b",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "cf5ceabe4804c9f7d275",
+    "url": "/Weather-app/static/css/2.fb449846.chunk.css"
+  },
+  {
+    "revision": "2f9fdd5c9495bc370e08",
+    "url": "/Weather-app/static/css/main.6ca63b20.chunk.css"
+  },
+  {
+    "revision": "cf5ceabe4804c9f7d275",
+    "url": "/Weather-app/static/js/2.379e7d99.chunk.js"
+  },
+  {
+    "revision": "2f9fdd5c9495bc370e08",
+    "url": "/Weather-app/static/js/main.176502ab.chunk.js"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
